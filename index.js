9ddfc235b098da9571949a30a9fd0ebf664abf9b
Ahmed Ali <aali@cinnova.com>
2024-01-10 13:12:47 +0500

    aaaaa

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-});
+var http = require('http');
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World! Node.js is working correctly.\n');
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
